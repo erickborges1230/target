6cd6348c98d7116cb5deb38389673f9ffff43568
Erick Borges <erickborges1230@gmail.com>
2026-02-20 19:54:43 -0300

    update file database

--- src/app/database/migrate.ts
+++ src/app/database/migrate.ts
@@ -2,28 +2,27 @@ import {type SQLiteDatabase} from "expo-sqlite";
 
 export async function migrate(database: SQLiteDatabase) {
   await database.execAsync(`
-            PRAGMA foreign_keys = ON;
+    PRAGMA foreign_keys = ON;
 
-            CREATE TABLE IF NOT EXISTS targets (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name TEXT NOT NULL,
-                amount FLOAT NOT NULL,
-                created_at timestamp NOT NULL DEFAULT current_timestamp,
-                updated_at timestamp NOT NULL DEFAULT current_timestamp
-            );
+    CREATE TABLE IF NOT EXISTS targets (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      amount REAL NOT NULL,
+      created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    );
 
-            CREATE TABLE IF NOT EXISTS transactions (
-                id INTERGER PRIMARY KEY AUTOINCREMENT,
-                target_id INTERGER NOT NULL,
-                amount FLOAT NOT NULL,
-                observation TEXT NULL,
-                
-                created_at timestamp NOT NULL DEFAULT current_timestamp,
-                updated_at timestamp NOT NULL DEFAULT current_timestamp,
-
-                CONSTRAINT fk_targets_transactions
-                FOREIGN KEY (target_id) REFERENCES targets(id)
-                ON DELETE CASCADE
-            );
-        `);
+    CREATE TABLE IF NOT EXISTS transactions (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      target_id INTEGER NOT NULL,
+      amount REAL NOT NULL,
+      observation TEXT,
+      created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      CONSTRAINT fk_targets_transactions
+        FOREIGN KEY (target_id)
+        REFERENCES targets(id)
+        ON DELETE CASCADE
+    );
+  `);
 }
